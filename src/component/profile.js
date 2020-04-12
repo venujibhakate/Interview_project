31eee746396ba0d9e5178a75e0bbaab223566e53
Your Name <you@example.com>
2020-04-12 17:58:02 +0530

    showed the github  user profile pic and username

--- src/component/profile.js
+++ src/component/profile.js
@@ -2,19 +2,35 @@ import React from 'react';
 import CardContent from '@material-ui/core/CardContent';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const Styles = theme => ({
+    image: {
+        width:'25ch',
+    },
+    mainDiv:{
+        marginLeft:'10%'
+    },
+    content:{
+        width:'22ch'
+    },
+    btn:{
+        width:'25ch'
+    }
+})
 
 function Profile(props) {
-	const {profile} = props;
+    const {profile,classes} = props;
+    // const{classes}= this.props
 	return (
-		<div>
-			{profile.map(profile => (
-				<Card style={{ width: '250px', marginLeft: '15%', marginTop: '1%' }}>
+		<div className={classes.mainDiv}>
+    		{profile.map(profile => (
+				<Card key="id" className={classes.image}>
 					<CardContent>
-						<img style={{ width: '200px', marginLeft: '5%' }} alt="thumbnail" src={profile.avatar_url} />
+						<img className={classes.content} alt="thumbnail" src={profile.avatar_url} />
 						<a href={profile.html_url} style={{ textDecoration: 'none' }}>
 							<Button
-								variant="contained"
-								style={{ marginTop: '25px', fontSize: '22', width: '100%' }}>
+								variant="contained" className={classes.btn}>
 								View Profile
 								</Button>
 						</a>
@@ -31,4 +47,4 @@ function Profile(props) {
 	);
 }
 
-export default Profile;
+export default (withStyles)(Styles)(Profile);
